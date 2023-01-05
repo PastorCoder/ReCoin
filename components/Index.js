94fabe8044dfab394ae0ef6b5865c16f17e118da
pastorcoder <zuma4good@gmail.com>
2023-01-05 02:47:12 +0100

    done with Button, SubInfo and NFTCard components

--- components/Index.js
+++ components/Index.js
@@ -1,10 +1,13 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
 
 
 export {
     HomeHeader,
     FocusedStatusBar,
-    NFTCard
+    NFTCard,
+    CircleButton,
+    RectButton,
 };
